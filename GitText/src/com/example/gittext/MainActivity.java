408395e54e7cc12f0b3d9c553552cce6e6685d18
xijie <xjjacy@gmail.com>
2013-05-29 15:49:20 +0800

    test

--- GitText/src/com/example/gittext/MainActivity.java
+++ GitText/src/com/example/gittext/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.gittext;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +10,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Toast.makeText(this, "this is a git test!", Toast.LENGTH_SHORT).show();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
